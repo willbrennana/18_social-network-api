e23e4da09442b65521ff17de61bb268169634578
willbrennana <will.brennana@gmail.com>
2023-03-02 22:00:42 -0600

    Added server.js and model files. Updated User.js and Reaction.js files based on schemas outlined in README.

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+import dateFormat from "../utils/dateFormat";
+
+const reactionSchema = new Schema(
+  {
+    reactionId: { type: Schema.Types.ObjectId, default: new Types.ObjectId() },
+    reactionBody: { type: String, required: true, maxLength: 280 },
+    username: { type: String, required: true },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    toJSON: {
+      // TODO: Mongoose will not include virtuals by default, so add a `virtuals` property and set it's value to true
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// TODO: Create a virtual property `fullName` on the userSchema
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Initialize our User model
+const User = model("user", userSchema);
+
+module.exports = User;
